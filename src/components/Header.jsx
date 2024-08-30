62bcc529e02cbd0ec9e0eacf329254d2f68ef43e
Isabeli <86092004+BelistarE@users.noreply.github.com>
2024-08-29 18:48:09 -0700

    updated button styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import { Button } from "@radix-ui/themes";
-import { Link } from "@nextui-org/react";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 const Header = () => {
+  const handleButtonClick = () => {
+    window.open("https://github.com/BelistarE", "_blank");
+  };
+
   return (
     <div className="header">
       <h1>cv.builder</h1>
-      <Button variant="surface" as={Link} href="https://github.com/BelistarE">
+      <Button
+        variant="surface"
+        onClick={handleButtonClick}
+        className="link-button"
+      >
         <GitHubLogoIcon />
         <p>Made by Isabeli Estefano</p>
       </Button>
